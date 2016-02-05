d6dc2d51355bb39c6bb8b1627bdac5925adfa982
tiikkala <tapioikkala@gmail.com>
2016-02-05 16:44:16 +0200

    tests updated

--- src/test/java/javalabra/shakki/engine/board/BoardUtilsTest.java
+++ src/test/java/javalabra/shakki/engine/board/BoardUtilsTest.java
@@ -17,22 +17,22 @@ public class BoardUtilsTest {
     
     @Test
     public void invalidTileCoordinateBelowZero() {
-        assertEquals(false, BoardUtils.isValidTileCoordinate(-1));
+        assertFalse(BoardUtils.isValidTileCoordinate(-1));
     }
     
     @Test
     public void invalidTileCoordinateOverSixtyThree() {
-        assertEquals(false, BoardUtils.isValidTileCoordinate(64));
+        assertFalse(BoardUtils.isValidTileCoordinate(64));
     }
     
     @Test
     public void isValidTileCoordinateValid() {
-        assertEquals(true, BoardUtils.isValidTileCoordinate(12));
+        assertTrue(BoardUtils.isValidTileCoordinate(12));
     }
     
     @Test
     public void isFirstColumnFirstColumn() {
-        assertEquals(true, BoardUtils.FIRST_COLUMN[0]);
+        assertTrue(BoardUtils.FIRST_COLUMN[0]);
     }
     
     @Test
@@ -42,17 +42,17 @@ public class BoardUtilsTest {
     
     @Test
     public void isFirsRowFirstRow() {
-        assertEquals(true, BoardUtils.FIRST_ROW[0]);
+        assertTrue(BoardUtils.FIRST_ROW[0]);
     }
     
     @Test
     public void isFirstRowisOnlyFirstRow() {
-        assertEquals(false, BoardUtils.FIRST_ROW[8]);
+        assertFalse(BoardUtils.FIRST_ROW[8]);
     }
        
     @Test
     public void positiontoCoordinateReturnsCorrectCoordinate() {
-        assertEquals((Integer) 8, BoardUtils.POSITION_TO_COORDINATE.get("a7"));
+        assertTrue(8 == BoardUtils.POSITION_TO_COORDINATE.get("a7"));
     }
 }
     
